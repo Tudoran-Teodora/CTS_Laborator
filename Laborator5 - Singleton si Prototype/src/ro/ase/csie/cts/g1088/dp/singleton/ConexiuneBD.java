fec6b04cbb67acbcbf0a1e703f71686f997b8079
Tudoran-Teodora <tudoranteodora18@stud.ase.ro>
2021-03-25 12:27:16 +0200

    Exemplu Singleton

---
+++ Laborator5 - Singleton si Prototype/src/ro/ase/csie/cts/g1088/dp/singleton/ConexiuneBD.java
@@ -0,0 +1,27 @@
+package ro.ase.csie.cts.g1088.dp.singleton;
+
+public class ConexiuneBD {
+
+	String ip;
+	String denumire;
+	
+	private static ConexiuneBD conexiune=null;
+	
+	private ConexiuneBD() {
+		
+	}
+
+	private ConexiuneBD(String ip, String denumire) {
+		System.out.println("Apel constructor");
+		this.ip = ip;
+		this.denumire = denumire;
+	}
+	
+	public static ConexiuneBD getConexiune() {
+		if(conexiune==null) {
+			//datele se pot prelua din fisiere de configurare
+			conexiune=new ConexiuneBD("10.0.0.1","cts");
+		}
+		return conexiune;
+	}
+}
