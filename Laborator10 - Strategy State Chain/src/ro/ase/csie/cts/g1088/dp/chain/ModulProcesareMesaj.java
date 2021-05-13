1552037b735416c05d86e30c08ef0d34de038bd9
Tudoran-Teodora <tudoranteodora18@stud.ase.ro>
2021-05-13 12:26:15 +0300

    Implementare Chain

--- Laborator10 - Strategy State Chain/src/ro/ase/csie/cts/g1088/dp/chain/ModulProcesareMesaj.java
+++ Laborator10 - Strategy State Chain/src/ro/ase/csie/cts/g1088/dp/chain/ModulProcesareMesaj.java
@@ -4,4 +4,10 @@ public abstract class ModulProcesareMesaj {
 
 	ModulProcesareMesaj next=null;
 	public abstract void procesareMesaj(MesajChat mesaj);
+	
+	public void setNext(ModulProcesareMesaj next) {
+		this.next = next;
+	}
+	
+	
 }
